0ad8872209802e38edd5c6c74faf9e9af6575aab
hewliyang <hewliyang@u.nus.edu>
2022-10-06 23:56:47 +0800

    Merge distance.py into utils and remove import_data.py

--- backend/utils.py
+++ backend/utils.py
@@ -24,6 +24,31 @@ header = {'AccountKey':API_KEY, 'accept':'application/json'}
 def get_distance(lat1, long1, lat2, long2):
     return distance.distance((lat1, long1), (lat2, long2)).km
 
+def avg_distance(start, end, loc):
+    return (distance.distance(start, loc).km + distance.distance(end, loc).km)/2
+
+def find_closest(cam, speed):
+    closest_dist = []
+    closest = []
+    for i in range(len(speed.LinkID)):
+        minDist = 1000
+        minLoc = 0
+        for j in range(len(cam.Latitude)):
+            start = (speed["Lat Start"][i], speed["Long Start"][i])
+            end = (speed["Lat End"][i], speed["Long End"][i])
+            loc = (cam["Latitude"][j], cam["Longitude"][j])
+            dist = avg_distance(start, end, loc)
+            if (dist < minDist):
+                minDist = dist
+                minLoc = j
+        closest_dist.append(minDist * 1000)
+        closest.append(cam["CameraID"][minLoc])
+
+    s2 = speed.copy()
+    s2["Closest Cam"] = closest
+    s2["dist (m)"] = closest_dist
+    return s2
+
 def get_datetime(message):
     s = re.search(r"\(\d+\/\d+\)\d+\:\d+", message).group()
     dt = datetime.datetime.strptime(s, "(%d/%m)%H:%M")

038e95cbcc6351573ca17c52d32651ee77db272c
Poto <enversionkiller@gmail.com>
2022-10-10 14:00:28 +0800

    updated init file to enable easier imports

---
+++ frontend/src/dashboard/components/__init__.py
@@ -0,0 +1,5 @@
+# Pulling in components from files in the curr dir to make import cleaner
+from button import button
+from footer import footer
+from header import header
+from nav import nav
---
+++ frontend/src/dashboard/pages/__init__.py
@@ -0,0 +1,3 @@
+from home import home
+from map import map
+from grid import grid

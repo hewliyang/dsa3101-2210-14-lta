e055baffabe21486351e6f5e7cba18d3b52bb42f
C-WeiJie <myownselfme@gmail.com>
2022-10-13 19:26:50 +0800

    update map

--- frontend/src/dashboard/pages/__init__.py
+++ frontend/src/dashboard/pages/__init__.py
@@ -1,5 +1,4 @@
 from home import home
 from map import map
 from grid import grid
-from folium_map import folium_map
 from gridDetailed import gridDetailed
--- frontend/src/dashboard/pages/map.py
+++ frontend/src/dashboard/pages/map.py
@@ -5,10 +5,10 @@ from dash import html
 import dash_bootstrap_components as dbc
 import folium
 import json
-import folium_map as fm
+import os
 # from components import header, nav
 
-app = dash.Dash(__name__)
+#app = dash.Dash(__name__)
 
 dash.register_page(
 	__name__,
@@ -16,8 +16,52 @@ dash.register_page(
 	title='Map'
 )
 
-dat = fm.get_data()
-fm.make_map(dat)
+def get_data():
+    #load JSON to dict
+    #url = ''
+    #r1 = requests.get(url) 
+    #data = json.loads(js)
+
+    #dummy data
+    data = {1:{"latitude": 1.290270,"longitude": 103.851959,"density": 35, "speed": 60, "prob": 0.7},
+            2:{"latitude": 1.292270,"longitude": 103.852959,"density": 35, "speed": 60, "prob": 0.8},
+            3:{"latitude": 1.280270,"longitude": 103.861959,"density": 35, "speed": 60, "prob": 0.2},
+            4:{"latitude": 1.280170,"longitude": 103.851659,"density": 35, "speed": 60, "prob": 0.6},}
+    return data
+
+
+
+def make_map(data):
+    m = folium.Map(location= [1.3521, 103.8198], zoom_start= 12)
+    for i in data.values():
+        c = "green"
+        if i['prob'] > 0.7:
+            c = "red"
+        elif i['prob'] > 0.3:
+            c = "orange"
+        den = i['density']
+        pro = i['prob']
+        lat = i['latitude']
+        lon = i['longitude']
+        #still need include img tag in pop_html
+        pop_html = f'''<html> 
+                    <head>The location is {lat}, {lon}</head>
+                    density is {den} </br>
+                    probability of traffic jam is {pro}
+                    </html>'''
+        iframe = folium.IFrame(pop_html)
+        folium.Marker([lat, lon],
+            popup=folium.Popup(iframe
+            , max_width=250,min_width=300),
+            icon=folium.Icon(color=c)
+            ).add_to(m)
+    if os.path.exists("folium_map.html"):
+        os.remove("folium_map.html")
+
+    m.save("folium_map.html")
+    
+dat = get_data()
+make_map(dat)
 
 map1 = html.Div(
     [
@@ -32,7 +76,7 @@ map1 = html.Div(
 )
 
 
-app.layout = dbc.Container(
+layout = dbc.Container(
     [
         #header func,
         html.Br(),
@@ -46,12 +90,13 @@ app.layout = dbc.Container(
     ]
 )
 
-@app.callback(Output('map', 'srcDoc'), 
-              Input('dummy_input', 'children'))
+@dash.callback(
+    Output('map', 'srcDoc'), 
+    Input('dummy_input', 'children'))
               
 def refresh_map(children):
-    dat = fm.get_data()
-    fm.make_map(dat)
+    dat = get_data()
+    make_map(dat)
     return open('folium_map.html','r').read()
     
 

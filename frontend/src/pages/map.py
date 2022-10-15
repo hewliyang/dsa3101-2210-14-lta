c624b86c6a30504283232245838304484d1251ce
C-WeiJie <myownselfme@gmail.com>
2022-10-15 22:02:49 +0800

    update map markers

--- frontend/src/pages/map.py
+++ frontend/src/pages/map.py
@@ -22,33 +22,47 @@ def get_data():
     data = {1:{"latitude": 1.290270,"longitude": 103.851959,"density": 35, "speed": 60, "prob": 0.7},
             2:{"latitude": 1.292270,"longitude": 103.852959,"density": 35, "speed": 60, "prob": 0.8},
             3:{"latitude": 1.280270,"longitude": 103.861959,"density": 35, "speed": 60, "prob": 0.2},
-            4:{"latitude": 1.280170,"longitude": 103.851659,"density": 35, "speed": 60, "prob": 0.6},}
+            4:{"latitude": 1.280170,"longitude": 103.851659,"density": 35, "speed": 60, "prob": 0.6},
+            4:{"latitude": 1.3521,"longitude": 103.8198,"density": 35, "speed": 60, "prob": 0.6}}
     return data
 
 def make_map(data):
     m = folium.Map(location= [1.3521, 103.8198], zoom_start= 12)
     for i in data.values():
-        c = "green"
-        if i['prob'] > 0.7:
-            c = "red"
-        elif i['prob'] > 0.3:
-            c = "orange"
+        #change to each direction
         den = i['density']
         pro = i['prob']
         lat = i['latitude']
         lon = i['longitude']
+        
+        c = "green"
+        if pro > 0.7:
+            c = "red"
+        elif pro > 0.3:
+            c = "orange"
+        
         #still need include img tag in pop_html
         pop_html = f'''<html> 
                     <head>The location is {lat}, {lon}</head>
+                    </br>
+                    <img src = "", alt = "camera image"/>
+                    </br>
+                    <section style = "background-color:#006877; display: inline-block; padding: 30px">
                     density is {den} </br>
                     probability of traffic jam is {pro}
+                    </section>
+                    <section style = "background-color:#6bf766; display: inline-block; padding: 30px">
+                    density is {den} </br>
+                    probability of traffic jam is {pro}
+                    </section>
                     </html>'''
         iframe = folium.IFrame(pop_html)
         folium.Marker([lat, lon],
             popup=folium.Popup(iframe
-            , max_width=250,min_width=300),
+            , max_width=550,min_width=600),
             icon=folium.Icon(color=c)
             ).add_to(m)
+        
     if os.path.exists("folium_map.html"):
         os.remove("folium_map.html")
 
@@ -61,8 +75,7 @@ map1 = html.Div(
     [
         dbc.Row(
             [
-                html.P("The Map"),
-                html.Iframe(id = "map", srcDoc = 'folium_map.html', width = '90%', height = '650')
+                html.Iframe(id = "map", srcDoc = 'folium_map.html', width = '100%', height = '750px')
                 
             ]
         )
@@ -73,8 +86,6 @@ map1 = html.Div(
 layout = dbc.Container(
     [
         #header func,
-        html.Br(),
-        html.H1('title'),
         html.Br(),
         #nav func,
         map1,

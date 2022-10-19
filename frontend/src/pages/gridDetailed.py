e4ad37613b9a74d1591a1532710bf864c22e0eb7
Poto <enversionkiller@gmail.com>
2022-10-19 12:38:51 +0800

    Clicked image works now, change last updated

--- frontend/src/pages/gridDetailed.py
+++ frontend/src/pages/gridDetailed.py
@@ -9,20 +9,21 @@ import os
 dash.register_page(
 	__name__,
 	path='/gridDetailed',
-	title='Grid Detailed View'
+	title='Grid Detailed View',
+	description="Shows the detailed description version of the chosen image"
 )
-
+random.seed(1)
 imgSrcList = [(file, round(random.uniform(0, 1), 2)) for file in os.listdir("src/assets/sampleImg/") if file.endswith(".jpg")]
 imgSrcList.sort(key= lambda x:x[1], reverse=True)
 
 # Color border based on probability
 def seriousness(probability):
 	if probability > 0.7:
-		return {"border":0, "outline":"2px solid red", "outline-offset":"-2px"}
+		return {"border":0, "outline":"4px solid red", "outline-offset":"-4px"}
 	elif probability > 0.3:
-		return {"border":0, "outline":"2px solid orange", "outline-offset":"-2px"}
+		return {"border":0, "outline":"4px solid orange", "outline-offset":"-4px"}
 	else:
-		return {"border":0, "outline":"2px solid green", "outline-offset":"-2px"}
+		return {"border":0, "outline":"4px solid green", "outline-offset":"-4px"}
 
 def create_card(img_src, severity, main=False):
 	image = f'./assets/sampleImg/{img_src}'
@@ -33,20 +34,20 @@ def create_card(img_src, severity, main=False):
 				dbc.CardImg(src=image, className = 'align-self-center', style={"max-height":"60vh", "height":"60vh"}),
 			], style = severity
 		)
-	else: #TODO: Send click image reference to detailed, so that it will be the main figure displayed
+	else: 
 		return dbc.Card(
 			[
-				#html.H4("Placeholder", style={'textAlign': 'center'}),
 				dbc.CardImg(src=image, className = 'align-self-center', style={"max-height":"25vh", "height":"auto"}),
 				dbc.CardImgOverlay(
-				[dbc.Button(href="http://127.0.0.1:8050/gridDetailed", 
+				[dbc.Button(href=f"http://127.0.0.1:8050/gridDetailed?main_picture={img_src}", 
 				style= {"opacity": 0, "height": "100%", "width": "100%", "margin":0, "padding":0, "border":0})
 				], 
 				style = {"padding":0, "margin":0})
 			], style = severity
 		)
 
-layout = html.Div(
+def layout(main_picture=None, **other_unknown_query_strings):
+	return html.Div(
 	[
 		# First Row of Traffic Images
 		dbc.Row(
@@ -90,17 +91,19 @@ layout = html.Div(
 			style = {"padding":0, "margin":0}
 		),
 		html.Div(id="last_updated_detailed", style = {"float":"right", "padding-right":"10px", "padding-top":"10px"}),
-		html.Div(id="time_detailed", style={"display":None}),
-		html.Div(id="selected_img", style={"display":None})
+		html.Div(id="time_detailed", style={"display":"None"}),
+		html.Div([dcc.Input(value=main_picture, id="selected_img", style={"display":"None"})], style={"display":"None"})
 	]
 )
 
 @callback(
 	Output(component_id='last_updated_detailed', component_property='children'),
 	Input(component_id='time_detailed', component_property='children')
-) #TODO: Change last_updated time to the time of the previous photo by extracting it from the image name
+)
+# 2706_1143_20220105114554_d54810
 def last_updated(time):
-	time = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") #Mock Data
+	pic = imgSrcList[0][0][10:22]
+	time = datetime.datetime.strptime(pic, "%Y%m%d%H%M") #Mock Data
 	return f'Last Updated: {time}'
 
 @callback(
@@ -109,10 +112,17 @@ def last_updated(time):
 	Output("img_b2_detailed", "children"), #a3
 	Output("img_b3_detailed", "children"), #a4
 	Output("img_b4_detailed", "children")],
-	Input("selected_img", "children")
-) #TODO: Identify selected_img using a tag from the clicked_image
+	Input("selected_img", "value")
+) 
 def update_images(selected_img):
-	imgToDisplay = [create_card(imgSrcList[0][0], seriousness(imgSrcList[0][1]), True)] # Main
+	main_value = 0
+	for i in range(len(imgSrcList)):
+		if imgSrcList[i][0] == selected_img:
+			main_value = i
+	# Main
+	imgToDisplay = [create_card(imgSrcList[main_value][0], seriousness(imgSrcList[main_value][1]), True)]
+	
+	
 	imgToDisplay += [create_card(imgSrcList[imgNum][0], seriousness(imgSrcList[imgNum][1])) for imgNum in range(1, 5)]
 	#imgToDisplay = [imgSrcList[clickedIndex]]
 	#for i in range(len(imgSrcList)):

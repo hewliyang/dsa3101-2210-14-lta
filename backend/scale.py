5a4f67ca58ddf8d21dec3814b6a8990c25407b6e
Quah Xi Wen <quahxiwen@gmail.com>
2022-10-22 15:28:24 +0800

    simplified the functions

--- backend/scale.py
+++ backend/scale.py
@@ -12,23 +12,12 @@ d1 = d["density1"].to_numpy()
 d2 = d["density2"].to_numpy()
 density = pd.to_numeric(np.append(d2,d1))
 
-#scaling data with outliers using minmax, standard, robust and maxabs
-
-min_max_scaler = MinMaxScaler()
-density1 = min_max_scaler.fit_transform(density.reshape(-1,1))
-df1 = pd.DataFrame(density1).rename({0: 'min_max'}, axis=1)
-
-std_scaler = StandardScaler()
-density2 = std_scaler.fit_transform(density.reshape(-1,1))
-df2 = pd.DataFrame(density2).rename({0: 'std_scale'}, axis=1)
-
-density3 = RobustScaler().fit_transform(density.reshape(-1,1))
-df3 = pd.DataFrame(density3).rename({0: 'robust_scale'}, axis=1)
-
-density4 = MaxAbsScaler().fit_transform(density.reshape(-1,1))
-df4 = pd.DataFrame(density4).rename({0: 'max_abs_scale'}, axis=1)
-
-with_outliers = pd.concat([df1, df2, df3, df4], axis=1)
+def scale(dat):
+  df1 = pd.DataFrame(MinMaxScaler().fit_transform(dat.reshape(-1,1))).rename({0: 'min_max'}, axis=1)
+  df2 = pd.DataFrame(StandardScaler().fit_transform(dat.reshape(-1,1))).rename({0: 'std_scale'}, axis=1)
+  df3 = pd.DataFrame(RobustScaler().fit_transform(dat.reshape(-1,1))).rename({0: 'robust_scale'}, axis=1)
+  df4 = pd.DataFrame(MaxAbsScaler().fit_transform(dat.reshape(-1,1))).rename({0: 'max_abs_scale'}, axis=1)
+  return pd.concat([df1, df2, df3, df4], axis=1)
 
 #to remove outliers
 iso = IsolationForest(contamination=0.001)
@@ -37,20 +26,8 @@ density_nonoutliers = density[yhat != -1]
 density_outliers = density[yhat == -1]
 num_outliers = np.size(density_outliers)
 
+#scaling data with outliers using minmax, standard, robust and maxabs
+with_outliers = scale(density)
+
 #scaling data without outliers using minmax, standard, robust and maxabs
-
-min_max_scaler = MinMaxScaler()
-densityno1 = min_max_scaler.fit_transform(density_nonoutliers.reshape(-1,1))
-no1 = pd.DataFrame(densityno1).rename({0: 'min_max'}, axis=1)
-
-std_scaler = StandardScaler()
-densityno2 = std_scaler.fit_transform(density_nonoutliers.reshape(-1,1))
-no2 = pd.DataFrame(densityno2).rename({0: 'std_scale'}, axis=1)
-
-densityno3 = RobustScaler().fit_transform(density_nonoutliers.reshape(-1,1))
-no3 = pd.DataFrame(densityno3).rename({0: 'robust_scale'}, axis=1)
-
-densityno4 = MaxAbsScaler().fit_transform(density_nonoutliers.reshape(-1,1))
-no4 = pd.DataFrame(densityno4).rename({0: 'max_abs_scale'}, axis=1)
-
-without_outliers = pd.concat([no1, no2, no3, no4], axis=1)
+without_outliers = scale(density_nonoutliers)
